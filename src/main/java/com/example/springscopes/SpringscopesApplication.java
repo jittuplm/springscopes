fb336697c45692c2d29bd03a6282d719c937f2fd
Jitendra Nath <jitendranath@MacBook-Pro.attlocal.net>
2020-06-20 13:21:04 -0500

    first commit

---
+++ src/main/java/com/example/springscopes/SpringscopesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springscopes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringscopesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringscopesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springscopes/SpringscopesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springscopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringscopesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
